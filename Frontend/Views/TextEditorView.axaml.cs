2f662331ef226c602745e0a8053d9dae99a5d34e
Alex <alexdennett16@gmail.com>
2026-01-30 14:37:03 +0000

    begin adding new text box i think this is right track?

--- Frontend/Views/TextEditorView.axaml.cs
+++ Frontend/Views/TextEditorView.axaml.cs
@@ -1,4 +1,7 @@
+using System;
+using Avalonia;
 using Avalonia.Controls;
+using ClearText.ViewModels;
 
 namespace ClearText.Views;
 
@@ -7,5 +10,28 @@ public partial class TextEditorView : UserControl
     public TextEditorView()
     {
         InitializeComponent();
+
+        // When DataContext changes, sync VM → Editor
+        _ = this.GetObservable(DataContextProperty)
+    .Subscribe(dc =>
+    {
+        if (dc is not TextEditorViewModel vm) return;
+        Editor.Text = vm.DocumentText;
+
+        Editor.TextChanged += (_, _) =>
+        {
+            if (Editor.Text != vm.DocumentText)
+                vm.DocumentText = Editor.Text;
+        };
+
+        vm.PropertyChanged += (_, e) =>
+        {
+            if (e.PropertyName == nameof(vm.DocumentText) &&
+                Editor.Text != vm.DocumentText)
+            {
+                Editor.Text = vm.DocumentText;
+            }
+        };
+    });
     }
 }

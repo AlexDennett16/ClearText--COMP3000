180651dbeee86b60d78e36e2a1173132d7f04be8
Alex <alexdennett16@gmail.com>
2026-02-06 16:05:28 +0000

    Squashed commit of the following:
    commit 272e067eda0e805ad60abb3b84c7f2ebd596ba35
    Author: Alex <alexdennett16@gmail.com>
    Date:   Fri Feb 6 16:04:59 2026 +0000
    
        seems like a decent implementation of given squigglies
    
    commit bf54d473f28ee447f0ccb7bdcf279d3e78a87fab
    Author: Alex <alexdennett16@gmail.com>
    Date:   Fri Feb 6 14:09:30 2026 +0000
    
        use capitals dummy
    
    commit 9b847a23f58cac501d3d768565b63c25e1e05ea8
    Author: Alex <alexdennett16@gmail.com>
    Date:   Fri Jan 30 16:41:54 2026 +0000
    
        getting there
    
    commit 2f662331ef226c602745e0a8053d9dae99a5d34e
    Author: Alex <alexdennett16@gmail.com>
    Date:   Fri Jan 30 14:37:03 2026 +0000
    
        begin adding new text box i think this is right track?

--- Frontend/Views/TextEditorView.axaml.cs
+++ Frontend/Views/TextEditorView.axaml.cs
@@ -1,11 +1,63 @@
+using System;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Media;
+using ClearText.Services;
+using ClearText.ViewModels;
 
 namespace ClearText.Views;
 
 public partial class TextEditorView : UserControl
 {
+    TextMarkerService _markerService;
+
     public TextEditorView()
     {
         InitializeComponent();
+
+        //Attach TextEditor for underline Squigglies
+        _markerService = new TextMarkerService(Editor.Document);
+
+        Editor.TextArea.TextView.BackgroundRenderers.Add(_markerService);
+
+
+
+        // When DataContext changes, sync VM → Editor
+        _ = this.GetObservable(DataContextProperty)
+    .Subscribe(dc =>
+    {
+        if (dc is not TextEditorViewModel vm) return;
+
+        //Text Loaded, can now load squigglies
+        Editor.Text = vm.DocumentText;
+
+        LoadSquigglies();
+
+
+        Editor.TextChanged += (_, _) =>
+        {
+            if (Editor.Text != vm.DocumentText)
+                vm.DocumentText = Editor.Text;
+            LoadSquigglies();
+        };
+
+        vm.PropertyChanged += (_, e) =>
+        {
+            if (e.PropertyName == nameof(vm.DocumentText) &&
+                Editor.Text != vm.DocumentText)
+            {
+                Editor.Text = vm.DocumentText;
+            }
+        };
+    });
+    }
+
+    private void LoadSquigglies()
+    {
+        _markerService.ClearMarkers();
+
+        _markerService.LoadSquigglies(Editor.Text);
+
+        Editor.TextArea.TextView.InvalidateVisual();
     }
 }

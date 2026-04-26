c6dad6da11ebbf1953e8e48051e4eb26a4063969
Alex <alexdennett16@gmail.com>
2026-04-26 15:08:25 +0100

    base viewmodels should dispose

--- Frontend/BaseTypes/BaseViewModels/ViewModelBase.cs
+++ Frontend/BaseTypes/BaseViewModels/ViewModelBase.cs
@@ -1,6 +1,13 @@
+using System;
 using ReactiveUI;
 
 namespace ClearText.BaseTypes.BaseViewModels;
 
-public class ViewModelBase : ReactiveObject;
+public class ViewModelBase : ReactiveObject, IDisposable
+{
+    public virtual void Dispose()
+    {
+        // Override in derived classes if needed
+    }
+};
 

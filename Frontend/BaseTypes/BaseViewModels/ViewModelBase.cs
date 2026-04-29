e1eb266d693055ab31fb34feaf04df355d73d68f
Alex <alexdennett16@gmail.com>
2026-04-29 23:40:43 +0100

    better dispose logic

--- Frontend/BaseTypes/BaseViewModels/ViewModelBase.cs
+++ Frontend/BaseTypes/BaseViewModels/ViewModelBase.cs
@@ -5,9 +5,19 @@ namespace ClearText.BaseTypes.BaseViewModels;
 
 public class ViewModelBase : ReactiveObject, IDisposable
 {
-    public virtual void Dispose()
+    private bool _disposed;
+    public void Dispose()
     {
-        // Override in derived classes if needed
+        if (_disposed) return;
+
+        Dispose(true);
+        GC.SuppressFinalize(this);
+        _disposed = true;
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        // Override in derived classes
     }
 };
 

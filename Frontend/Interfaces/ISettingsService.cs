4adf3d111ec65f6cb0d1bdc23006512fd0a4d648
Alex <alexdennett16@gmail.com>
2026-05-09 12:55:12 +0100

    Tidy up settings service

--- Frontend/Interfaces/ISettingsService.cs
+++ Frontend/Interfaces/ISettingsService.cs
@@ -1,13 +1,10 @@
 using ClearText.DataObjects;
-using ClearText.Enums;
-using DocumentFormat.OpenXml.Wordprocessing;
 
 namespace ClearText.Interfaces;
 
 public interface ISettingsService
 {
     SettingsConfig Config { get; }
-    void LoadSettings();
     void UpdateSettings(SettingsConfig config);
     SettingsConfig CreateWorkingCopy();
 }
